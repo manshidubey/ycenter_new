28aa2d03568679af48e84f5f04f000f8d2d9e6ad
manshi <manshi.neha@gmail.com>
2017-11-02 15:19:04 +0530

    story cards responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,20 @@ console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55')
         });
         wow.init();
 
+        //Count nr. of square classes
+  var countSquare = $('.square').length;
+
+  //For each Square found add BG image
+  for (i = 0; i < countSquare; i++) {
+    var firstImage = $('.square').eq([i]);
+    var secondImage = $('.square2').eq([i]);
+
+    var getImage = firstImage.attr('data-image');
+    var getImage2 = secondImage.attr('data-image');
+
+    firstImage.css('background-image', 'url(' + getImage + ')');
+    secondImage.css('background-image', 'url(' + getImage2 + ')');
+  }
 
         /* ---------------------------------------------- /*
          * Scroll top
